eafc9540a4ebcb877d5c98cbe8248d6fc2c0ef97
lmikovic <stojadin78@gmail.com>
2018-03-22 22:35:30 +0100

    Update login page

--- Code9Xamarin/Code9Xamarin/Code9Xamarin.Android/MainActivity.cs
+++ Code9Xamarin/Code9Xamarin/Code9Xamarin.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
 
 namespace Code9Xamarin.Droid
 {
@@ -21,6 +22,7 @@ namespace Code9Xamarin.Droid
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
+            Xamarin.Forms.Application.Current.On<Xamarin.Forms.PlatformConfiguration.Android>().UseWindowSoftInputModeAdjust(WindowSoftInputModeAdjust.Resize);
         }
     }
 }

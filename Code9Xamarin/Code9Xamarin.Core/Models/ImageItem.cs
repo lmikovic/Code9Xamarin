3d10311685e9286f2a171cb39fb1f09fc433b74a
lmikovic <stojadin78@gmail.com>
2018-03-25 21:34:39 +0200

    Implement INotifyPropertyChanged in ImageItem

--- Code9Xamarin/Code9Xamarin.Core/Models/ImageItem.cs
+++ Code9Xamarin/Code9Xamarin.Core/Models/ImageItem.cs
@@ -1,18 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Xamarin.Forms;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace Code9Xamarin.Core.Models
 {
-    public class ImageItem
+    public class ImageItem : INotifyPropertyChanged
     {
         public int Id { get; set; }
         public string UserName { get; set; }
         public DateTime DateTime { get; set; }
         public string Description { get; set; }
-        public int LikesNumber { get; set; }
         public int CommentsNumber { get; set; }
         public string ImageSource { get; set; }
+
+        private int _likesNumber;
+        public int LikesNumber
+        {
+            get
+            {
+                return _likesNumber;
+            }
+            set
+            {
+                if (value != _likesNumber)
+                {
+                    _likesNumber = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
